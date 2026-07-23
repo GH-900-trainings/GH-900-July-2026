31c03183180c3c59f5d4f79afa15b0b36047cfe1
Nuttapong Wong-aree <nuwongaree@microsoft.com>
2026-07-23 13:17:03 +0800

    Repo Setup & Base Server

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-import { createServer } from 'node:http';
+import express from 'express';
 
+const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-  res.end('<h1>Hello, World!</h1><p>Running on Node.js ' + process.version + '</p>');
+app.get('/', (req, res) => {
+  res.send(`<h1>Hello, World!</h1><p>Running on Node.js ${process.version}</p>`);
 });
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
